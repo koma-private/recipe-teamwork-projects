5e316ab62d91b9328bffca6b0d855666457bdb72
koma <mkoma@K.local>
2017-11-07 02:35:17 +0900

    remove unnecessary variable

--- webview.js
+++ webview.js
@@ -3,14 +3,13 @@
 module.exports = (Franz, options) => {
   function getMessages() {
     let directCount = 0;
-    let indirectCount = 0;
     let badge = document.getElementById('numNotifs2');
 
     if (badge && badge.innerText) {
-      indirectCount = parseInt(badge.innerText);
+      directCount = parseInt(badge.innerText);
     }
 
-    Franz.setBadge(directCount, indirectCount);
+    Franz.setBadge(directCount);
   }
 
   Franz.loop(getMessages);
