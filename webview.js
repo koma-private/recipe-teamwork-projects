676302fff8dd4f377d3ad6c30ddaf0e226f371ad
Masanori KOMAGAMINE <github@ringo.mm.st>
2018-06-29 15:13:18 +0900

    Fix counting notification, support Team Icon

--- webview.js
+++ webview.js
@@ -1,16 +1,50 @@
 'use strict';
+var _electron = require('electron');
+var _path = require('path');
+var _path2 = _interopRequireDefault(_path);
 
-module.exports = (Franz, options) => {
-  function getMessages() {
-    let directCount = 0;
-    let badge = document.getElementsByClassName('w-notification__count-label');
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+}
 
-    if (badge.length > 0 && badge[0].innerText) {
-      directCount = parseInt(badge[0].innerText);
-    }
+const getTeamIcon = function getTeamIcon(count = 0) {
+  let countTeamIconCheck = count;
+  let bgUrl = null;
 
-    Franz.setBadge(directCount);
+  window.location.href = "#dashboard";
+
+  const icon = document.querySelector('div.mlogo img');
+  if (icon) {
+    bgUrl = icon.getAttribute('src');
   }
 
+  countTeamIconCheck += 1;
+
+  if (bgUrl) {
+    _electron.ipcRenderer.sendToHost('avatar', bgUrl);
+  } else if (countTeamIconCheck <= 5) {
+    setTimeout(() => {
+      getTeamIcon(countTeamIconCheck + 1);
+    }, 2000);
+  }
+};
+
+module.exports = (Franz) => {
+  const getMessages = () => {
+    let directCount = 0;
+    const badge = document.querySelector('.w-notification__count-label');
+
+    if (badge.innerText) {
+      directCount = parseInt(badge.innerText, 10);
+    }
+    Franz.setBadge(directCount);
+  };
+
   Franz.loop(getMessages);
+
+  setTimeout(() => {
+    getTeamIcon();
+  }, 4000);
 }
